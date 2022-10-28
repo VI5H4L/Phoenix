4ff3c77cccb9760f6fb4aff5e3b3f27e399a2354
Vishal Kumar <21ucc116@lnmiit.ac.in>
2022-10-28 14:53:35 +0530

    Blog Changes

--- src/Components/Blogmaincomponentup.js
+++ src/Components/Blogmaincomponentup.js
@@ -4,17 +4,17 @@ export default function Blogmaincomponent(){
         <>
         <div className={blogupcss.maincontainer}>
             <div className={blogupcss.maincontainerleft} >
-            <img className={blogupcss.containerimage} src="images/BLOG-IMG.PNG" alt="BLOG-IMG" />
+            <img className={blogupcss.containerimage} src="images/blog0.jpg" alt="BLOG-IMG" />
             </div>
             <div className={blogupcss.maincontainerright}>
             <p  className={blogupcss.blogdate} >
-              21 June 2022
+              25 April 2022
             </p>
             <h2 className={blogupcss.blogheading} >
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur suscipit eius impedit nostrum eveniet nobis 
+            Torque ' 22 
             </h2>
             <p className={blogupcss.blogsubheading}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatibus, magnam dolorum facilis quidem saepe incidunt porro voluptate s
+            The new batch of Y-21 was introduced to the basics of Robotics with the help of Differential Drive Robot. It was followed by Track completion and Robosoccer competitions with the bots that they bulit after the informative introduction.
             </p>
             </div>
         </div>

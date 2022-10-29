6384b31cc73f0c5eacff51a5ba38d5f602c3f0ca
Vishal Kumar <21ucc116@lnmiit.ac.in>
2022-10-29 16:37:25 +0530

    Single Blog Page Added

--- src/Components/SingleBlog.js
+++ src/Components/SingleBlog.js
@@ -3,8 +3,11 @@ import SBlogCSS from "./SingleBlog.module.css"
 import { useParams } from 'react-router-dom'
 import Blogdata from "./Blogdata";
 import TopBlogdata from "./TopBlogdata"
+import {IoMdArrowRoundBack} from "react-icons/io"
+import { useNavigate } from 'react-router-dom';
 
 const SingleBlog = () => {
+    let navigate = useNavigate();
     let {blogID} = useParams();
     let isTopBlog = false;
     if(blogID==="0"){
@@ -12,7 +15,11 @@ const SingleBlog = () => {
     }
   return (
     <div className={SBlogCSS.main}>
-        <img className={SBlogCSS.mainImage} src={isTopBlog?TopBlogdata[0].url:Blogdata[blogID-1].url} alt=""></img>
+        <div className={SBlogCSS.imageDiv}>
+            <div onClick={()=>{navigate("/blogs")}}><IoMdArrowRoundBack size={18} className={SBlogCSS.iconStyle}/></div>
+            <img className={SBlogCSS.mainImage} src={isTopBlog?TopBlogdata[0].url:Blogdata[blogID-1].url} alt=""></img>
+        </div>
+        
         <p className={SBlogCSS.date}>{isTopBlog?TopBlogdata[0].date:Blogdata[blogID-1].date}</p>
         <p className={SBlogCSS.heading}>{isTopBlog?TopBlogdata[0].heading:Blogdata[blogID-1].heading}</p>
         <p className={SBlogCSS.subheading}>{isTopBlog?TopBlogdata[0].subheading:Blogdata[blogID-1].subheading}</p>

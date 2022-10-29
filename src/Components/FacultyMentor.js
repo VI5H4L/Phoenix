e51d8ee603a8280b755be652ab736169280dce6d
Vishal Kumar <21ucc116@lnmiit.ac.in>
2022-10-29 19:47:10 +0530

    compress imgs

---
+++ src/Components/FacultyMentor.js
@@ -0,0 +1,11 @@
+import Fcmen from './FacultyMentor.module.css';
+
+function FacultyMentor() {
+    return(
+         <div className={Fcmen.container}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut praesentium dignissimos culpa voluptatibus commodi animi quo enim cum suscipit officia.
+         </div>
+    );
+}
+
+export default FacultyMentor;

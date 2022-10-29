6b5ae5eee60c74d9c4d5873edb065eb2ee52bb44
Vishal Kumar <21ucc116@lnmiit.ac.in>
2022-10-29 21:25:35 +0530

    Faculty Section

--- src/Components/FacultyMentor.js
+++ src/Components/FacultyMentor.js
@@ -1,10 +1,20 @@
-import Fcmen from './FacultyMentor.module.css';
+import fcmen from './FacultyMentor.module.css';
+import profileImage from '../images/dr-abhishek-sharma.jpeg';
 
 function FacultyMentor() {
-    return(
-         <div className={Fcmen.container}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut praesentium dignissimos culpa voluptatibus commodi animi quo enim cum suscipit officia.
-         </div>
+    return (
+        <div className={fcmen.container}>
+            <div className={fcmen.header}>Our Faculty Mentor</div>
+            <div className={fcmen.subsection}>
+                <div className={fcmen.image}>
+                    <img src={profileImage} alt="Dr. Abhishek Sharma" />
+                </div>
+                <div className={fcmen.name}>Dr. Abhishek Sharma</div>
+                <div className={fcmen.desc}>
+                    Assistant Professor <br />
+                    Electronics & Communication Department</div>
+            </div>
+        </div>
     );
 }
 
